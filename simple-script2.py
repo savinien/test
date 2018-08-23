e3b8504852947a7ac359037af2c482452f3c9b22
SAVINIEN Jean <savinien@mac20087-3.local>
2018-08-23 14:20:33 +0200

    branch-2: updating README.md and adding a new python script

---
+++ simple-script2.py
@@ -0,0 +1,22 @@
+# test python script
+import matplotlib
+import matplotlib.pyplot as plt
+plt.style.use("ggplot")
+matplotlib.rcParams.update({"font.size": 14})
+
+k_values = [1,2,3,4,5,6]
+coherences = [0.21, 0.23, 0.24, 0.22, 0.25, 0.24]
+# line plot
+ax = plt.plot(k_values, coherences)
+plt.xticks(k_values)
+plt.xlabel("Number of Topics")
+plt.ylabel("Mean Coherence")
+# add the points
+plt.scatter(k_values, coherences, s=120)
+# find and annotate the maximum point on the plot
+ymax = max(coherences)
+xpos = (coherences).index(ymax)
+best_k = k_values[xpos]
+plt.annotate( "k=%d" % best_k, xy=(best_k, ymax), xytext=(best_k, ymax), textcoords="offset points", fontsize=16)
+# show the plot
+plt.show()
